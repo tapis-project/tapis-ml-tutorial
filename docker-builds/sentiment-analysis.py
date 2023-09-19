3579a560478943daeba822f54e2db6371b083791
Steve Black <scblack@tacc.utexas.edu>
2023-09-19 16:21:25 -0500

    Update Dockerfile and python code so single image can run under docker and singularity.

--- docker-builds/sentiment-analysis.py
+++ docker-builds/sentiment-analysis.py
@@ -18,11 +18,27 @@ result = sentiment_analyzer(args)
 
 # Print output to screen and write to file
 tapis_app_id = os.environ.get('_tapisAppId')
-print('_tapisAppId: %s\n' % tapis_app_id)
 if tapis_app_id is None:
+    # Not running under Tapis, write to /tmp
+    print('Not running under Tapis. Writing output to /tmp\n')
     outputPath = '/tmp/output.txt'
 else:
-    outputPath = '/TapisOutput/output.txt'
+    # Running under Tapis.
+    print('Running under Tapis: _tapisAppId=%s\n' % tapis_app_id)
+    # If Docker, write to /TapisOutput,
+    # else write to $_tapisExecSystemOutputDir
+    tapis_sing_name = os.environ.get('SINGULARITY_NAME')
+    if tapis_sing_name is None:
+        print('Running via Tapis using Docker\n')
+        outputPath = '/TapisOutput/output.txt'
+    else:
+        print('Running via Tapis using Singularity\n')
+        outputDir = os.environ.get('_tapisExecSystemOutputDir')
+        if outputDir is None:
+            print('WARNING: _tapisExecSystemOutputDir not set. Using /tmp for output.')
+            outputPath = '/tmp/output.txt'
+        else:
+            outputPath = outputDir + "/output.txt"
 print('Using output path: %s\n' % outputPath)
 f = open(outputPath, "a")
 print(str(result))
